a816b3970b627d1b49ff2283fed985d52209ac06
Abelsabater <abesabmun@alu.edu.gva.es>
2024-11-08 12:59:43 +0100

    Metodos

--- src/Siglas.java
+++ src/Siglas.java
@@ -1,16 +1,16 @@
 public class Siglas {
 
-    public static String siglas(String frase){
-        String [] cadena= frase.split(" ");
+    public static String siglas(String []frase){
         String siglas="";
-        for (int i = 0; i < cadena.length; i++) {
-            siglas+=cadena[i].charAt(0);
+        for (int i = 0; i < frase.length; i++) {
+            siglas+=frase[i].charAt(0);
         }
         return siglas.toUpperCase();
     }
 
     public static void main(String[] args) {
         String frase= "Hola aço es una frase";
-        System.out.println(siglas(frase));
+        String [] cadena= frase.split(" ");
+        System.out.println(siglas(cadena));
     }
 }
