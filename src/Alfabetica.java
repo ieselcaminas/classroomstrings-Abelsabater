70b3cdb7ac20055f35d4509e5dc1e66816a6ee2a
Abelsabater <abesabmun@alu.edu.gva.es>
2024-11-08 13:15:28 +0100

    Metodos

--- src/Alfabetica.java
+++ src/Alfabetica.java
@@ -1,12 +1,24 @@
 public class Alfabetica {
     public static boolean alfabetica(String palabra){
         boolean alfabetica=false;
-
+        int valor=0;
+        for (int i = 0; i < palabra.length(); i++) {
+            if(palabra.charAt(i)>valor){
+                valor=palabra.charAt(i);
+                alfabetica=true;
+            }else{
+                alfabetica=false;
+                break;
+            }
+        }
 
         return alfabetica;
     }
 
     public static void main(String[] args) {
-
+        String palabra="mina";
+        String palabra2="amor";
+        System.out.println(alfabetica(palabra.toUpperCase()));
+        System.out.println(alfabetica(palabra2.toUpperCase()));
     }
 }
