4e0d84a3bb64cde41c3dc3787bc79ba8c0dc9544
Abelsabater <abesabmun@alu.edu.gva.es>
2024-11-08 11:12:37 +0100

    Metodos

--- src/FraseInvertida.java
+++ src/FraseInvertida.java
@@ -1,5 +1,5 @@
 public class FraseInvertida {
-    public static String fraseinvertida(String frase){
+    public static String invertir(String frase){
         String cadena="";
         String [] fras=frase.split(" ");
         for (int i = fras.length -1;i>=0;--i) {
@@ -9,6 +9,6 @@ public class FraseInvertida {
     }
     public static void main(String[] args) {
         String cadena="Esto es una frase";
-        System.out.println(fraseinvertida(cadena));
+        System.out.println(invertir(cadena));
     }
 }
