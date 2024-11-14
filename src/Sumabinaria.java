27435a80b1b9ef8b3154627590b82c53ce198528
Abelsabater <abesabmun@alu.edu.gva.es>
2024-11-14 14:28:00 +0100

    Metodos

--- src/Sumabinaria.java
+++ src/Sumabinaria.java
@@ -1,8 +1,6 @@
 public class Sumabinaria {
     public static String sumabinaria(String  bin1, String  bin2){
-        int decimal1=Integer.parseInt(bin1,2);
-        int decimal2=Integer.parseInt(bin2,2);
-        int sumad=decimal1+decimal2;
+        int sumad=Integer.parseInt(bin1,2)+Integer.parseInt(bin2,2);
         String suma=Integer.toBinaryString(sumad);
         return suma;
     }
