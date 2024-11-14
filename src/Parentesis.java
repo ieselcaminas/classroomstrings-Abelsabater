6e29661488eb43dbcdd1b5cee5b1ef7e62afd522
Abelsabater <abesabmun@alu.edu.gva.es>
2024-11-14 12:16:36 +0100

    Metodos

--- src/Parentesis.java
+++ src/Parentesis.java
@@ -4,14 +4,17 @@ public class Parentesis {
         boolean parentizada=false;
         int oberta=0;
         int tancada=0;
+        String ultima="";
         for (int i = 0; i < texto.length(); i++) {
             if(texto.charAt(i)=='('){
                 oberta++;
+                ultima="(";
             } else if (texto.charAt(i)==')') {
                 tancada++;
+                ultima=")";
             }
         }
-        if (oberta==tancada){
+        if (oberta==tancada && ultima==")" ){
             parentizada=true;
         }
 
@@ -20,7 +23,7 @@ public class Parentesis {
     }
     public static void main(String[] args) {
         String texto="Esto (es (un ejemplo) (de) una (cadena bien) parentizada)";
-        String texto2="una )cadena (mal (parentizada) y (una() no lo esta";
+        String texto2="una )cadena (mal parentizada y una no lo esta";
         System.out.println(parentesis(texto.toLowerCase()));
         System.out.println(parentesis(texto2.toLowerCase()));
     }
